c8f66c7e765f044285e8f79a5e6669d305c2f4c6
Yoshi K <kai.outsider.777+git@gmail.com>
2026-03-01 18:56:38 +0900

    upd footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,11 @@
-import styles from "./footer.module.css";
-
 const Footer = () => {
-  return <div className={`${styles.footer}`}></div>;
+  return (
+    <footer className="bg-neutral-800 text-white py-8">
+      <div className="max-w-[720px] mx-auto px-4">
+        <div className="text-center"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
