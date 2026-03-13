ce60ddb0258a454b9abe47b53e9d754aff818412
Yoshi K <kai.outsider.777+git@gmail.com>
2026-03-13 23:34:42 +0900

    mv markdown style to markdown.js

--- components/markdown.js
+++ components/markdown.js
@@ -7,7 +7,17 @@ const Markdown = ({ children, components, ...props }) => {
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw]}
-      {...(components && { components })}
+      components={{
+        ul: ({ children }) => (
+          <ul className="list-disc pl-6 flex flex-col gap-1">{children}</ul>
+        ),
+        ol: ({ children }) => (
+          <ol className="list-decimal pl-6 flex flex-col gap-1">{children}</ol>
+        ),
+        em: ({ children }) => <em className="text-neutral-500">{children}</em>,
+        p: ({ children }) => <p className="mb-4">{children}</p>,
+        ...components,
+      }}
       {...props}
     >
       {children}
